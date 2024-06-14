d4016e6f21fd657020ddb8d13c5d79f7559585bb
Napoleao <luknapoleao@hotmail.com>
2024-06-13 21:39:36 -0300

    Criei um componente com o pacote formulario e estilizei ele para ficar mais organizado e poder aplicar mais fácilmente a estilização com o css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
-      <Banner />
-      <CampoTexto />
-      
+      <Banner />  
+      <Formulario />
     </div>
   );
 }

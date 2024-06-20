c4551b14d75f89b01c71effc8cdff64ccea149fb
Napoleao <luknapoleao@hotmail.com>
2024-06-20 11:02:15 -0300

    Implementei agora os cards com nome e imagem do colaboradores, inseri um filtro para designar o colaborador para o time certo.

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,14 +1,10 @@
 import './Banner.css'
 
-
-function Banner() {
-   
+export const Banner = () => {
     // JSX
     return (
         <header className="banner">
-        <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
         </header>
     )
 }
-
-export default Banner

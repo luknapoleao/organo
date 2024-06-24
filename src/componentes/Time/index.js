3d112ab45aeb6c51238c3a9485e0566d702efdc3
Napoleao <luknapoleao@hotmail.com>
2024-06-24 13:42:03 -0300

    Depurando elementos do código e incluindo rodapé

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,18 +2,17 @@ import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
-    
     const css = { backgroundColor: props.corSecundaria }
-    
+
     return (
-        
-        <section className='time' style={css}> 
-            <h3 style={{borderColor: props.corPrimaria }}>{props.nome}</h3>
-           <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador key={colaborador.nome} corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
             </div>
-        </section>
+        </section> 
+        : ''
+    )
+}
 
-    ) }
-
-    export default Time
+export default Time
